318e3494bd25ef8c2438575a32f06dec769a4b77
skuzema <s.kuzema@gmail.com>
2023-06-11 20:50:10 +0300

    feat: streams read, write

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import path from 'path';
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const sourceFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const readableStream = createReadStream(sourceFile);
+    await pipeline(readableStream, process.stdout);
+  } catch (err) {
+    throw new Error('Operation failed');
+  }
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import path from 'path';
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const targetFile = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  try {
+    const writableStream = createWriteStream(targetFile, { flags: 'a' });
+    await pipeline(process.stdin, writableStream);
+  } catch (err) {
+    throw new Error('Operation failed');
+  }
 };
 
 await write();

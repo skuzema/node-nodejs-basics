8d993d673b245b85379df6487f78d9b85e354c64
skuzema <s.kuzema@gmail.com>
2023-06-11 17:21:31 +0300

    feat: implement file system functions

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { writeFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
+const fileContent = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(filePath, fileContent, { flag: 'wx' });
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const sourceFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const contents = await readFile(sourceFile, { encoding: 'utf8' });
+    console.log(contents);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();

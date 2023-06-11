98cb15c2706b999840b28242bd1713ef06cddd7c
skuzema <s.kuzema@gmail.com>
2023-06-11 20:08:21 +0300

    feat: implements hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import path from 'path';
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const sourceFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const content = await readFile(sourceFile);
+    const data = createHash('sha256').update(content).digest('hex');
+    console.log(data);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await calculateHash();

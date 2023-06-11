97cdfc752fb6a753d26d178b5f950decd65da1e7
skuzema <s.kuzema@gmail.com>
2023-06-11 19:09:59 +0300

    feat: implement command line interface functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,22 @@
+const ARG_PREFIX = '--';
+
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const result = {};
+  let stringArgs = '';
+
+  for (let i = 0; i < args.length; i += 2) {
+    if (args[i].startsWith(ARG_PREFIX)) {
+      const key = args[i].replace(/^--/, '');
+      const value = args[i + 1];
+      result[key] = value;
+    }
+  }
+  for (const [key, value] of Object.entries(result)) {
+    stringArgs =
+      stringArgs + (stringArgs.length === 0 ? '' : ', ') + `${key} is ${value}`;
+  }
+  console.log(stringArgs);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+const prefix = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
+    const regex = new RegExp(`^${prefix}`);
+    const rssVariables = Object.entries(process.env)
+      .filter(([key]) => regex.test(key))
+      .map(([key, value]) => `${key}=${value}`)
+      .join('; ');
+
+    console.log(rssVariables);
 };
 
 parseEnv();

a2d7529cb1fd1dc17e80729d725edd788e48608d
skuzema <s.kuzema@gmail.com>
2023-06-15 20:55:25 +0300

    feat: implement zlib functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,31 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const sourceFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+const compressedFile = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const readStream = fs.createReadStream(sourceFile);
+    const writeStream = fs.createWriteStream(compressedFile);
+    const gzipStream = zlib.createGzip();
+
+    readStream.pipe(gzipStream).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+      console.log('File compressed successfully.');
+    });
+
+    writeStream.on('error', (error) => {
+      console.error('Error compressing file:', error);
+    });
+  } catch (err) {
+    throw new Error('Operation failed');
+  }
 };
 
 await compress();

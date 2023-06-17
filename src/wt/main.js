ac84a5dbb85fb5a0fbc16fd64cfbd0d6d00fe02c
skuzema <s.kuzema@gmail.com>
2023-06-17 12:06:46 +0300

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import { Worker } from 'node:worker_threads';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import os from 'os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const workerFile = path.join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCores = os.cpus().length;
+  const workers = Array.from({ length: numCores }, (_, i) => {
+    return new Promise((resolve) => {
+      const initialValue = 10 + i;
+      const worker = new Worker(workerFile, { workerData: initialValue });
+      worker.on('message', (result) =>
+        resolve({ status: 'resolved', data: result })
+      );
+      worker.on('error', () => resolve({ status: 'error', data: null }));
+    });
+  });
+  const results = await Promise.all(workers);
+  console.log(results);
 };
 
 await performCalculations();
